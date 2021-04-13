3781f81fd30879ac002aed1b7163de9de14d2bcd
Lalit Behera <lalit143.er@gmail.com>
2021-04-13 22:20:27 +0530

    static inner class demo

--- src/main/java/com/lkb/Test.java
+++ src/main/java/com/lkb/Test.java
@@ -2,6 +2,32 @@ package com.lkb;
 
 public class Test {
     public static void main(String[] args) {
+        A aa = new A();
+        System.out.println(aa);
+        A.InnerB b = aa.getInnerB();
+        //b.a = aa;
+        aa = null;
+        A ab = (A) b.a;
+        System.out.println(aa);
+        System.out.println(ab);
+        System.out.println(b.a);
         System.out.println("Hello");
     }
+}
+
+
+class A{
+    public A(){
+    }
+
+    public InnerB getInnerB(){
+        InnerB bb = new InnerB();
+        bb.a = this;
+        return bb;
+    }
+
+    static final class InnerB{
+        Object a;
+    }
+
 }
